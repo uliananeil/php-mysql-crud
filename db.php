3bae95c57f24473edac8c5fa378ab2f6845b3255
Uliana Neiliuk <60117819+uliananeil@users.noreply.github.com>
2024-07-19 20:28:47 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,17 @@
 <?php
 session_start();
 
+$db_host = getenv('DB_HOST');
+$db_user = getenv('DB_USER');
+$db_password = getenv('DB_PASSWORD');
+$db_name = getenv('DB_NAME');
+
 $conn = mysqli_connect(
-  'localhost',
-  'root',
-  'password123',
-  'php_mysql_crud'
-) or die(mysqli_erro($mysqli));
+  $db_host,
+  $db_user,
+  $db_password,
+  $db_name
+) or die(mysqli_error($conn));
 
 ?>
+
